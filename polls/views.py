b6460101052219807cb12a06b8c0f1b525f624d8
sunlei <1255193087@qq.com>
2023-08-01 17:25:23 +0800

    添加模板

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,26 @@
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World! You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {
+        "latest_question_List": latest_question_list,
+    }
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
